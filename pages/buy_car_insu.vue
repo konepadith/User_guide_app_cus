0b36359d9463e8ab6a6b7703a050a77f255f7675
Tonxay <Toninpased@gmail.com>
2024-01-04 17:34:37 +0700

    user guide custiomer

---
+++ pages/buy_car_insu.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col v-for="(item, idx) in images" :key="idx" cols="12">
+          <img :src="require(`@/assets/by_car_insurance/${item.name}.png`)" alt="Local Image" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        { name: '0' },
+        { name: '1' },
+        { name: '2' },
+        { name: '3' },
+        { name: '4' },
+        { name: '5' },
+        { name: '6' },
+        { name: '7' },
+        { name: '8' },
+        { name: '9' },
+        { name: '10' },
+        { name: '11' },
+        { name: '12' },
+        { name: '13' },
+        { name: '14' },
+        { name: '15' },
+        { name: '16' },
+        { name: '17' },
+        { name: '18' },
+        { name: '19' },
+        { name: '20' },
+        { name: '21' },
+        { name: '22' },
+        { name: '23' },
+        { name: '24' },
+        { name: '25' },
+        // Add more items as needed
+      ],
+    };
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,11 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12" md="6">
-        Your content for the first column  
-        Your content for the first column 
-      </v-col>
-      <v-col cols="12" md="6">
-      Your content for the first column 
-      </v-col>
-    </v-row>
-    <div>
+  
+
      <div style="width: 50%; float: left;">
-      <p>This is the first column.</p>
+      <p>MSIG SOKXAY INSURANCE</p>
      </div>
-     <div style="width: 50%; float: left;">
-      <p>This is the second column.</p>
-     </div>
-    </div>
+   
   </v-container>
   
 </template>
