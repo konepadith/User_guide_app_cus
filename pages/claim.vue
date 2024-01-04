535c6de20d1a76727a64bca5eb509752c476880d
Tonxay <Toninpased@gmail.com>
2024-01-04 17:12:05 +0700

    start project

---
+++ pages/claim.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col v-for="(item, idx) in images" :key="idx" cols="12">
+          <img :src="require(`@/assets/claim/${item.name}.png`)" alt="Local Image" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        { name: '0' },
+        { name: '1' },
+        { name: '2' },
+        { name: '3' },
+        { name: '4' },
+        { name: '5' },
+        { name: '6' },
+        // Add more items as needed
+      ],
+    };
+  },
+};
+</script>
---
+++ pages/tracking.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col v-for="(item, idx) in images" :key="idx" cols="12">
+          <img :src="require(`@/assets/tracking/${item.name}.png`)" alt="Local Image" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        { name: '0' },
+        { name: '1' },
+        { name: '2' },
+        { name: '3' },
+      
+        // Add more items as needed
+      ],
+    };
+  },
+};
+</script>
